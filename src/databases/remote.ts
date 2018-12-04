0ae58cea1204a00bf6154386186da6c5669e2d59
ozgursel <ozgurselr@gmail.com>
2018-12-04 19:59:13 +0300

    PouchDB Http Adapter Changes

--- src/databases/remote.ts
+++ src/databases/remote.ts
@@ -1,14 +1,16 @@
 import Nano from 'nano';
 import PouchDB from 'pouchdb-core';
 import PouchDBFind from 'pouchdb-find';
+import PouchDBHttp from 'pouchdb-adapter-http';
 import { Database } from '../models/management/database';
 
 PouchDB.plugin(PouchDBFind);
+PouchDB.plugin(PouchDBHttp);
 
 export const CouchDB = (database: Database) => {
     return Nano(`http://${database.username}:${database.password}@${database.host}:${database.port}`);
 }
 
 export const RemoteDB = (database: Database, collection: string) => {
-    return new PouchDB(`http://${database.username}:${database.password}@${database.host}:${database.port}/${collection}`);
+    return new PouchDB(`http://${database.username}:${database.password}@${database.host}:${database.port}/${collection}`, { adapter: 'http' });
 }
